24e03ca15ea832ee61bc3fe047139fef778ac997
narolaharsh <narola.bharatbhai@ldas-pcdev5.ligo.caltech.edu>
2020-04-22 23:51:02 -0700

    input script

---
+++ template-bank-generation/non-GR-bank/input10.sh
@@ -0,0 +1,3 @@
+pycbc_make_sbank_workflow -d /home/narola.bharatbhai/workflow-bank-non-GR-small/output10/out \
+	--workflow-name work10-nonGR --output-file attempt10-nonGR.h5 \
+	--config-files sbank-non-GR-10.ini
