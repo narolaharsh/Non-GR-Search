69840977180d9ae37bc9e930799f04cc4de89f96
narolaharsh <narola.bharatbhai@ldas-pcdev5.ligo.caltech.edu>
2020-04-23 08:34:07 -0700

    triggers params

---
+++ hw-inj/multiple-injections/triggers.sh
@@ -0,0 +1,28 @@
+pycbc_inspiral --segment-end-pad 64  \
+--segment-length 256 \
+--segment-start-pad 64 \
+--psd-estimation median \
+--psd-segment-length 16 \
+--psd-segment-stride 8 \
+--psd-inverse-length 16 \
+--pad-data 8 \
+--sample-rate 1024 \
+--low-frequency-cutoff 40 \
+--strain-high-pass 30 \
+--filter-inj-only \
+--processing-scheme cpu \
+--cluster-method template \
+--approximant IMRPhenomDpseudoFourPN \
+--order 8 \
+--snr-threshold 5.5 \
+--chisq-bins 16 \
+--channel-name H1:GDS-CALIB_STRAIN \
+--gps-start-time 1126368017 \
+--gps-end-time 1130371217 \
+--trig-start-time 1126368015 \
+--trig-end-time 1130371215 \
+--frame-type H1_HOFT_C00 \
+--injection-file HL-INJECTIONS_3-1126368017-4003200.xml \
+--bank-file Feb12.h5 \
+--output multiple_injections_output.h5 \
+--verbose
