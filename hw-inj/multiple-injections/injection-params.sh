edca185626f0a6fb37cafaf0f1b051f752d9ec4b
narolaharsh <narola.bharatbhai@ldas-pcdev5.ligo.caltech.edu>
2020-04-26 04:31:28 -0700

    time step changed

--- hw-inj/multiple-injections/injection-params.sh
+++ hw-inj/multiple-injections/injection-params.sh
@@ -1,11 +1,11 @@
-lalapps_inspinj --gps-start-time 1126368017 \
-    --gps-end-time 1126972817 \
+lalapps_inspinj --gps-start-time 1126086042 \
+    --gps-end-time 1126431642 \
     --f-lower 30.0 \
     --min-mtotal 10.0 --max-mtotal 100.0 \
     --min-spin1 0.0 --max-spin1 0.998 \
     --min-spin2 0. --max-spin2 0.998 \
     --min-mratio 1.0 --max-mratio 10.0 \
-    --i-distr fixed --fixed-inc 0 \
+    --i-distr uniform --fixed-inc 0 \
     --m-distr totalMassRatio \
     --min-distance 1000. \
     --max-distance 1000. \
@@ -15,9 +15,9 @@ lalapps_inspinj --gps-start-time 1126368017 \
     --enable-spin \
     --d-distr uniform \
     --time-step 600 \
-    --time-interval 650 \
+    --time-interval 25 \
     --seed 3
 
-#1126368017 2015-09-15T16:00:00
-#1126972817 2015-09-22T16:00:00
+#1126086042 2015-09-12T09:40:25
+#1126431642 2015-09-16T09:40:25
 #--time-interval 0. \
