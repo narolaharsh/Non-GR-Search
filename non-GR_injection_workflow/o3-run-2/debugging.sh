8d18d54af91ed0088afb3024671d486ab8209b03
narolaharsh <narola.bharatbhai@ldas-pcdev5.ligo.caltech.edu>
2020-06-03 04:51:55 -0700

    error file from this ini files

---
+++ non-GR_injection_workflow/o3-run-2/debugging.sh
@@ -0,0 +1,6 @@
+/home/narola.bharatbhai/src/nonGR-pycbc/bin/pycbc_plot_singles_vs_params --min-snr 6 --x-var end_time --y-var template_duration --z-var density \
+--bank-file /home/narola.bharatbhai/Non-GR-search/non-GR_injection_workflow/o3-run-2/output/local-site-scratch/work/./o3_workflow_inj_2-main_ID0000001/./H1L1-BANK2HDF-1126086042-345600.hdf \
+--veto-file /home/narola.bharatbhai/Non-GR-search/non-GR_injection_workflow/o3-run-2/output/local-site-scratch/work/./o3_workflow_inj_2-main_ID0000001/./H1L1-FOREGROUND_CENSOR-1126086042-345600.xml \
+--segment-name closed_box --detector L1 \
+--single-trig-file /home/narola.bharatbhai/Non-GR-search/non-GR_injection_workflow/o3-run-2/output/local-site-scratch/work/./o3_workflow_inj_2-main_ID0000001/./L1-HDF_TRIGGER_MERGE_FULL_DATA-1126086042-345600.hdf \
+--output-file L1-PLOT_SINGLES_ENDTIME_DURATION_DENSITY_FULL_DATA-1126086042-345600.png 
---
+++ non-GR_injection_workflow/o3-run-2/run.sh
@@ -0,0 +1,24 @@
+WORKFLOW_NAME=o3_workflow_inj_2
+CONFIG_TAG='v1.16.1'
+GPSSTART=1126086042
+GPSEND=1126431642
+CHUNKNUMBER=01
+#GITLAB_URL="https://git.ligo.org/ligo-cbc/pycbc-config/raw/${CONFIG_TAG}/O3C00/pipelineHL"
+
+ecp-cookie-init LIGO.ORG https://git.ligo.org/users/auth/shibboleth/callback narola.bharatbhai
+
+pycbc_make_coinc_search_workflow \
+  --workflow-name ${WORKFLOW_NAME} --output-dir output \
+  --config-files \
+  analysis.ini \
+  executables.ini \
+  injections_minimal.ini \
+  plotting.ini \
+  data_O3_C00.ini \
+  gating.ini \
+  gps_times.ini \
+  --config-overrides workflow:start-time:${GPSSTART} workflow:end-time:${GPSEND} \
+      'results_page:analysis-subtitle:"O3 Analysis '${CHUNKNUMBER}', C00 data"' \
+      results_page:output-path:"/home/${USER}/public_html/o3/runs/hl/c00/${WORKFLOW_NAME}" \
+      workflow:file-retention-level:all_triggers
+
