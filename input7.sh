71985e26581255a76ed32b25483b4afc3240f890
narolaharsh <narola.bharatbhai@ldas-pcdev5.ligo.caltech.edu>
2020-06-12 23:39:39 -0700

    config and run file

---
+++ input7.sh
@@ -0,0 +1,3 @@
+pycbc_make_sbank_workflow -d /home/narola.bharatbhai/workflow-bank/attempt7/out7 \
+ 	--workflow-name work7 --output-file /home/narola.bharatbhai/workflow-bank/attempt7/out7/Feb12.h5 \
+	--config-files GR-bank-7.ini
